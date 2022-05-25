cb103a3484942dab44eea98cdead7fa0b51b3117
David Nee <davidni055@gmail.com>
2022-05-26 00:43:57 +0800

    modify registerpage with some additional components

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,13 +2,14 @@ import React from "react";
 
 export const Input = React.forwardRef(
   (
-    { className, name, placeholder, type = "text", children, ...restProps },
+    { className, style, name, placeholder, type = "text", children, ...restProps },
     ref
   ) => {
     return (
       <>
         <input
           ref={ref}
+          style={{width: "1000px" }}
           className={className}
           type={type}
           name={name}
--- src/pages/RegisterPage/index.js
+++ src/pages/RegisterPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 import { Row } from "components/Row";
@@ -7,6 +7,8 @@ import { Image } from "components/Image";
 import { Text } from "components/Text";
 import { Button } from "components/Button";
 import { Stack } from "components/Stack";
+import { Input } from "components/Input";
+import { SelectBox } from "components/SelectBox";
 
 const RegisterPagePage = () => {
   const navigate = useNavigate();
@@ -25,20 +27,18 @@ const RegisterPagePage = () => {
             alt="MINERVALOGO1"
           />
           <Text className="font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[32px] xl:mt-[37px] mt-[42px] 3xl:mt-[50px] not-italic lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-gray_901 text-left">{`Username`}</Text>
-          <Text className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left">{`Enter username`}</Text>
+          <Input className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left" placeholder="Enter username"></Input>
           <Text className="font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] mt-[11px] 3xl:mt-[13px] lg:mt-[8px] xl:mt-[9px] not-italic lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-gray_901 text-left">{`Email`}</Text>
-          <Text className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left">{`Enter email`}</Text>
+          <Input className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left" placeholder="Enter email"></Input>
           <Text className="font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[12px] xl:mt-[14px] mt-[16px] 3xl:mt-[19px] not-italic lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-gray_901 text-left">{`Password`}</Text>
-          <Text className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left">{`Enter password`}</Text>
+          <Input type="password" className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left" placeholder="Enter password"></Input>
           <Text className="font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[12px] xl:mt-[14px] mt-[16px] 3xl:mt-[19px] not-italic lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-gray_901 text-left">{`Confirm password`}</Text>
-          <Text className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left">{`Enter password`}</Text>
+          <Input type="password" className="bg-gray_100 border border-bluegray_100 border-solid font-normal lg:ml-[51px] xl:ml-[58px] ml-[66px] 3xl:ml-[79px] lg:mr-[44px] xl:mr-[50px] mr-[57px] 3xl:mr-[68px] lg:mt-[4px] xl:mt-[5px] mt-[6px] 3xl:mt-[7px] not-italic lg:pl-[12px] xl:pl-[14px] pl-[16px] 3xl:pl-[19px] lg:py-[3px] py-[4.5px] 2xl:py-[4px] xl:py-[4px] 3xl:py-[5px] rounded-radius12 text-bluegray_500 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left" placeholder="Confirm password"></Input>
           <Text className="font-normal 3xl:mt-[10px] lg:mt-[7px] xl:mt-[8px] mt-[9px] lg:mx-[51px] xl:mx-[58px] mx-[66px] 3xl:mx-[79px] not-italic text-black_900 lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33 text-left">{`Country`}</Text>
           <Column className="items-center lg:mb-[19px] xl:mb-[22px] mb-[25px] 3xl:mb-[30px] lg:mt-[10px] xl:mt-[11px] mt-[13px] 3xl:mt-[15px] w-[100%]">
-            <Image
-              src="img_inputdropdown.svg"
+            <SelectBox
               className="lg:h-[35px] xl:h-[41px] h-[45px] 2xl:h-[46px] 3xl:h-[55px] mx-[auto] object-contain rounded-radius12 w-[85%]"
-              alt="Inputdropdown"
-            />
+              ></SelectBox>
             <Button className="bg-blue_A700 border border-blue_A700 border-solid font-bold lg:mt-[31px] xl:mt-[36px] mt-[41px] 3xl:mt-[49px] mx-[auto] lg:py-[24px] xl:py-[27px] py-[31px] 3xl:py-[37px] rounded-radius12 text-center lg:text-fs24 xl:text-fs28 text-fs32 3xl:text-fs38 text-white_A700 tracking-ls1 w-[89%]">{`Sign up`}</Button>
             <Row className="items-end justify-center mt-[1px] mx-[auto] w-[45%]">
               <Text className="font-normal xl:mb-[2px] lg:mb-[2px] mb-[3px] xl:mt-[10px] mt-[12px] 3xl:mt-[14px] lg:mt-[9px] not-italic text-black_900 text-center lg:text-fs21 xl:text-fs24 text-fs28 3xl:text-fs33">{`Have an account?`}</Text>
@@ -49,10 +49,10 @@ const RegisterPagePage = () => {
             </Row>
           </Column>
         </Column>
-        <Stack className="font-merriweather 3xl:h-[1025px] lg:h-[664px] xl:h-[759px] h-[853px] 2xl:h-[854px] w-[43%]">
+        <Stack className="font-merriweather 3xl:h-[1025px] lg:h-[500px] xl:h-[1000px] h-[1000px] 2xl:h-[1000px] w-[43%]">
           <Image
             src="img_image5.png"
-            className="absolute 3xl:h-[1025px] lg:h-[664px] xl:h-[759px] h-[853px] 2xl:h-[854px] inset-[0] object-cover w-[100%]"
+            className="absolute 3xl:h-[1025px] lg:h-[500px] xl:h-[700px] h-[1000px] 2xl:h-[600px] inset-[0] object-cover w-[100%]"
             alt="image5"
           />
           <Column className="absolute inset-x-[0] items-start justify-start mx-[auto] top-[5%] w-[86%]">

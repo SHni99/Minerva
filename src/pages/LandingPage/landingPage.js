35b0252fbd6eb1a5b410b584d754b2d6b056e000
chunzkok <kokcz@comp.nus.edu.sg>
2022-06-05 00:36:39 +0800

    Added Why Minerva section

--- src/pages/LandingPage/landingPage.js
+++ src/pages/LandingPage/landingPage.js
@@ -7,27 +7,33 @@ import FooterBar from "components/FooterBar/footerBar";
 
 import landingPageStyles from "./landingPage.module.css";
 
-const LandingPagePage = () => {
+const LandingPage = () => {
   return (
-    <Container
-      fluid
-      className="p-0 gx-0"
-      style={{
-        backgroundImage: `url(${require("assets/images/img_books1.png")})`,
-        backgroundSize: "cover",
-      }}
-    >
+    <Container fluid className="p-0 gx-0">
       <IntroSection />
+      <WhyMinervaSection />
       <FooterBar />
     </Container>
   );
 };
 
-export default LandingPagePage;
+export default LandingPage;
 
-const IntroSection = (props) => {
+// Used to simplify process of getting image link of an image under src/assets/images
+const srcImgLink = (imgName) => {
+  return `url("${require("assets/images/" + imgName)}")`;
+};
+
+const IntroSection = () => {
   return (
-    <Container className="px-md-3 px-lg-5" fluid>
+    <Container
+      className="px-md-3 px-lg-5"
+      fluid
+      style={{
+        backgroundImage: srcImgLink("img_books1.png"),
+        backgroundSize: "cover",
+      }}
+    >
       {/* This row contains the header of the document. (i.e. Logo + Listing/Login/Register buttons) */}
       <Row className="justify-between g-0 py-3">
         {/* Minerva Logo */}
@@ -60,8 +66,8 @@ const IntroSection = (props) => {
       </Row>
 
       {/* Text portion + two buttons */}
-      <Row className="px-5 pt-6 justify-center justify-content-sm-start">
-        <Col xs={12} sm={8} md={5} lg={4} className="ps-sm-3 ps-lg-4">
+      <Row className="px-5 py-8 justify-center justify-content-md-start">
+        <Col xs={12} sm={7} md={5} lg={4} className="ps-sm-3 ps-lg-4">
           <Row>
             <h1 className={landingPageStyles["intro-header"]}>
               Can't find suitable tutors/tutees?
@@ -81,15 +87,142 @@ const IntroSection = (props) => {
                 See How It Works
               </Button>
             </Col>
-            <Col xs={12} md={4} className="d-flex justify-center my-2">
+            <Col xs={12} md="auto" className="d-flex justify-center my-2">
               <Button
                 className="p-3 border"
                 variant="dark"
                 style={{ borderRadius: "10px" }}
+                href="/aboutuspage"
               >
                 About Us
               </Button>
             </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+const WhyMinervaSection = () => {
+  return (
+    <Container className="bg-dark px-5" fluid>
+      {/* << Why Minerva >> Header */}
+      <Row className="py-5">
+        <h1
+          className={`text-light text-center ${landingPageStyles["whyMinerva-heading"]}`}
+        >
+          WHY MINERVA?
+        </h1>
+      </Row>
+
+      {/* Reason 1: Direct Matching */}
+      <Row className="justify-center">
+        <Col xs={12} md={7} className="d-flex">
+          <img
+            src={require("assets/images/img_communicationd.png")}
+            alt="Two individuals communicating directly"
+          />
+        </Col>
+        <Col xs={7} md={5} className="d-flex flex-column justify-center">
+          <Row>
+            <p
+              className={`${landingPageStyles["whyMinerva-sub1"]} text-center text-md-start`}
+            >
+              FAST & EFFICIENT
+            </p>
+          </Row>
+          <Row>
+            <h2
+              className={`${landingPageStyles["whyMinerva-1"]} text-center text-md-start`}
+            >
+              DIRECT MATCHING
+            </h2>
+          </Row>
+          <Row>
+            <p
+              className={`${landingPageStyles["whyMinerva-text1"]} text-center text-md-start`}
+            >
+              Chat directly with your chosen tutor/tutee!
+              <br />
+              Avoid delays caused by manual matching processes by agencies.
+            </p>
+          </Row>
+        </Col>
+      </Row>
+
+      {/* Reason 2: User-friendly Interfaces*/}
+      <Row className="justify-center flex-row-reverse">
+        <Col xs={12} md={5} className="d-flex justify-center align-center py-5">
+          <img
+            src={require("assets/images/img_happysunshine.png")}
+            alt="Happy Sunshine"
+          />
+        </Col>
+        <Col xs={7} className="d-flex flex-column justify-center">
+          <Row>
+            <p
+              className={`${landingPageStyles["whyMinerva-sub1"]} text-center text-md-end`}
+            >
+              EASY TO USE
+            </p>
+          </Row>
+          <Row>
+            <h2
+              className={`${landingPageStyles["whyMinerva-1"]} text-center text-md-end`}
+            >
+              USER-FRIENDLY INTERFACES
+            </h2>
+          </Row>
+          <Row>
+            <p
+              className={`${landingPageStyles["whyMinerva-text1"]} text-center text-md-end`}
+            >
+              Say goodbye to messy walls of text.
+              <br />
+              Minerva enables easy filtering of listings through clean and
+              simple menus.
+              <br />
+              Make use of the intuitive and straightforward controls to make
+              your experience better!
+            </p>
+          </Row>
+        </Col>
+      </Row>
+
+      {/* Reason 3: Highly Customisable*/}
+      <Row className="justify-center">
+        <Col xs={12} md={5} className="d-flex justify-center align-center pb-5">
+          <img
+            src={require("assets/images/img_multitaskingma.png")}
+            alt="Happy Sunshine"
+          />
+        </Col>
+        <Col xs={7} className="d-flex flex-column justify-center">
+          <Row>
+            <p
+              className={`${landingPageStyles["whyMinerva-sub1"]} text-center text-md-start`}
+            >
+              FLEXIBLE
+            </p>
+          </Row>
+          <Row>
+            <h2
+              className={`${landingPageStyles["whyMinerva-1"]} text-center text-md-start`}
+            >
+              HIGHLY CUSTOMISABLE
+            </h2>
+          </Row>
+          <Row>
+            <p
+              className={`${landingPageStyles["whyMinerva-text1"]} text-center text-md-start`}
+            >
+              Want to teach for free? Only able to teach certain topics of a
+              subject? Wish to only teach online?
+              <br />
+              Fret not! Minerva can handle all these requests. Simply add more
+              fields and let Minerva do the rest.
+            </p>
           </Row>
         </Col>
       </Row>

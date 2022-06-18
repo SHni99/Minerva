c5fec91a9e164c0abbce25482234268ad3673e0c
chunzkok <kokcz@comp.nus.edu.sg>
2022-06-18 16:50:19 +0800

    Added Listing Modal display which appears whenever a Listing Card is clicked. To be integrated with future features (profile page, chat + reviews)

---
+++ src/components/ListingModal/listingModal.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import listingModalStyles from "./listingModal.module.css";
+import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FieldTag from "components/FieldTag/fieldTag";
+
+const ListingModal = (props) => {
+  const { onHide, data } = props;
+  const { show, username, avatarUrl, image_urls, fields, creator_id } = data;
+  const tagNames = {
+    subject: "Subjects",
+    commitment: "Commitment Period",
+    qualifications: "Qualifications",
+    timing: "Preferred Timings",
+    others: "Others",
+  };
+
+  const tags = fields.reduce((acc, obj) => {
+    const key = obj.category;
+    acc[key] = [...(acc[key] || []), obj.value];
+    return acc;
+  }, {});
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <img
+          src={avatarUrl || "/images/img_avatarDefault.jpg"}
+          alt="Avatar"
+          className={listingModalStyles.avatar}
+        />
+        <a href="/" className="nunito-sans">
+          {username}
+        </a>
+        {/* Add rating here! (stars) */}
+      </Modal.Header>
+      <Modal.Body>
+        <Row className={`${listingModalStyles["image-section"]} pb-4`}>
+          {!image_urls || image_urls.length < 1 ? (
+            <h1 className="text-center ">No listing images!</h1>
+          ) : (
+            <Carousel
+              variant="dark"
+              indicators={false}
+              controls={image_urls.length > 1}
+              interval={null}
+            >
+              {image_urls.map((imgUrl) => (
+                <Carousel.Item
+                  className={listingModalStyles["carousel-item"]}
+                  key={imgUrl}
+                >
+                  <img src={imgUrl} alt="Listing Details" />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          )}
+        </Row>
+        {Object.keys(tags).map((tag) => (
+          <Row className={listingModalStyles["tag-row"]}>
+            <Col
+              className={listingModalStyles["tag-header"]}
+              xs={5}
+            >{`${tagNames[tag]}: `}</Col>
+            <Col xs={7}>
+              <FieldTag category={tag} value={tags[tag]} key={tag} />
+            </Col>
+          </Row>
+        ))}
+      </Modal.Body>
+      <Modal.Footer className="px-4 d-flex justify-content-evenly">
+        <Button variant="outline-secondary">View Profile</Button>
+        <Button className="px-5">Chat</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ListingModal;

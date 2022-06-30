c24bfb01da8b79b6b2b089db862ee6d408ef2615
chunzkok <kokcz@comp.nus.edu.sg>
2022-06-30 18:42:24 +0800

    Added permanent floating toast on bottom right corner of screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import Routes from "./Routes";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ToastContainer position="bottom-end" className="position-fixed">
+        <Toast>
+          <Toast.Header>hi</Toast.Header>
+          <Toast.Body>Hi</Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </>
+  );
 }
 
 export default App;

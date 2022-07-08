4f52633770344befa1ec167f064e382a89a8fe1a
chunzkok <kokcz@comp.nus.edu.sg>
2022-07-09 01:25:05 +0800

    [Tests] Landing Page - Everything Else

--- src/pages/LandingPage/__test__/landingPage.test.js
+++ src/pages/LandingPage/__test__/landingPage.test.js
@@ -113,5 +113,111 @@ describe("Introduction Section", () => {
         expect(history.location.pathname).toBe("/registerpage");
       });
     });
+
+    describe("My Profile Button", () => {
+      const getProfileButton = () =>
+        screen.getByRole("button", { name: /my profile/i });
+
+      it("is absent when user is not logged in", () => {
+        // Mocking non-logged-in user
+        mockUser(null);
+        wrapPage();
+        expect(
+          screen.queryByRole("button", { name: /my profile/i })
+        ).not.toBeInTheDocument();
+      });
+
+      it("is visible when user is logged in", () => {
+        // Mocking logged-in user
+        mockUser({ id: "a9fbf7d6-e8fb-4985-956c-f4e9dfb5cd0e" });
+        wrapPage();
+        expect(getProfileButton()).toBeInTheDocument();
+      });
+
+      it("redirects users to the profile page", () => {
+        // Mocking logged-in user
+        mockUser({ id: "a9fbf7d6-e8fb-4985-956c-f4e9dfb5cd0e" });
+        const history = wrapPage();
+        fireEvent.click(getProfileButton());
+        expect(history.location.pathname).toBe("/profile");
+      });
+    });
+
+    describe("Create Listings Button", () => {
+      const getCreateListingsButton = () =>
+        screen.getByRole("button", { name: /create listings/i });
+
+      it("is absent when user is not logged in", () => {
+        // Mocking non-logged-in user
+        mockUser(null);
+        wrapPage();
+        expect(
+          screen.queryByRole("button", { name: /create listings/i })
+        ).not.toBeInTheDocument();
+      });
+
+      it("is visible when user is logged in", () => {
+        // Mocking logged-in user
+        mockUser({ id: "a9fbf7d6-e8fb-4985-956c-f4e9dfb5cd0e" });
+        wrapPage();
+        expect(getCreateListingsButton()).toBeInTheDocument();
+      });
+
+      it("redirects users to the create listings page", () => {
+        // Mocking logged-in user
+        mockUser({ id: "a9fbf7d6-e8fb-4985-956c-f4e9dfb5cd0e" });
+        const history = wrapPage();
+        fireEvent.click(getCreateListingsButton());
+        expect(history.location.pathname).toBe("/create-listing");
+      });
+    });
+  });
+
+  describe("Section Body", () => {
+    describe("See How It Works Button", () => {
+      const getButton = () =>
+        screen.getByRole("button", { name: /see how it works/i });
+
+      it("is visible", () => {
+        wrapPage();
+        expect(getButton()).toBeInTheDocument();
+      });
+
+      it("refers user to the Get Started section", () => {
+        wrapPage();
+        expect(getButton()).toHaveAttribute("href", "#GetStarted");
+      });
+    });
+
+    describe("About Us Button", () => {
+      const getButton = () => screen.getByRole("button", { name: /about us/i });
+
+      it("is visible", () => {
+        wrapPage();
+        expect(getButton()).toBeInTheDocument();
+      });
+
+      it("redirects users to the About Us Page", () => {
+        wrapPage();
+        expect(getButton()).toHaveAttribute("href", "/aboutuspage");
+      });
+    });
+  });
+});
+
+describe("Get Started Section", () => {
+  describe("Sign Up Now Button", () => {
+    const getButton = () =>
+      screen.getByRole("button", { name: /sign up now/i });
+
+    it("is visible", () => {
+      wrapPage();
+      expect(getButton()).toBeInTheDocument();
+    });
+
+    it("redirects users to the Register Page", () => {
+      wrapPage();
+      expect(getButton()).toHaveAttribute("href", "/registerpage");
+    });
   });
 });

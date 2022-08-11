57b5919b298b526df176dabe4fc2c5e8662669cd
chunzkok <kokcz@comp.nus.edu.sg>
2022-08-11 22:01:41 +0800

    [Cypress] Lengthened timeout to facilitate for unusually sluggish Supabase response

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000",
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 20000,
     experimentalSessionAndOrigin: true,
   },
 });
